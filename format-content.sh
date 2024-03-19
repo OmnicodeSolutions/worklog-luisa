291fd44c1bd4b379b1b517393d0d947139ec1cb6
Luísa Coelho <114317672+LuCCoelho@users.noreply.github.com>
2024-03-19 11:38:06 -0300

    chore: dont delete directory

--- format-content.sh
+++ format-content.sh
@@ -6,7 +6,6 @@ for i in content/blog_pt_br/*.md; do
     mv -- "$i" "${i%.md}.pt.md";
     mv content/blog_pt_br/*.pt.md content/blog/
 done
-rmdir content/blog_pt_br
 
 for i in content/blog/20*; do
     sed -i "4s/+++/\n[taxonomies]\nauthors = [\"$author\"]\n+++/1" $i
